b14fedd1741f2af5cd2c3c526888c1aabb44273a
Jimmy <jimmylefrancois38@gmail.com>
2017-04-26 13:55:06 +0200

    styles

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <div class="jumbotron">
 	<div class="container">
 		<h1>Hello, world!</h1>
+		<hr>
 		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
 
 		<hr>
@@ -10,7 +11,7 @@
 		<div class="row items_container">
 
 			<?php foreach ($folders as $index => $folder): ?>
-				<div class="col-md-4 item">
+				<div class="col-md-3 item">
 					<h2><?php echo ucfirst($index); ?></h2>
 
 					<p class="img_container">
@@ -19,7 +20,7 @@
 							<img src="<?php echo $folder['pic'] ?>" alt="Album <?php echo $index; ?>">
 						</a>
 					</p>
-					<p><a class="btn btn-default" href="albums/<?php echo $index ?>" role="button" title="Voir l'album <?php echo ucfirst($index) ?>">Voir l'album</a></p>
+					<p><a class="btn btn-default link" href="albums/<?php echo $index ?>" role="button" title="Voir l'album <?php echo ucfirst($index) ?>">Voir l'album</a></p>
 				</div>
 			<?php endforeach ?>
 
