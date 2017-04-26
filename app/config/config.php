82807412886750d903a21c6d3bc69a9c81b35a83
Jimmy <jimmylefrancois38@gmail.com>
2017-04-26 15:19:00 +0200

    path config

--- app/config/config.php
+++ app/config/config.php
@@ -4,5 +4,5 @@ $dir = 'albums/';
 
 $domain = $_SERVER[HTTP_HOST];
 
-if ($domain = 'ca-dev.fr') $filePath = 'http://ca-dev.fr/PERSO/example_outdoor/';
+if ($domain = 'ca-dev.fr') $filePath = 'http://ca-dev.fr/PERSO/example/';
 $asRoot = file_exists('albums');
