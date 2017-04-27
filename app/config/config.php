3baa1fb65ec253d4b97c5794db84d2e44b9e75e2
jimmy lefrancois <jimmylefrancois38@gmail.com>
2017-04-27 18:43:16 +0200

    Path edit

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,9 @@
 
 $dir = 'albums/';
 
-$domain = $_SERVER[HTTP_HOST];
+$domain = $_SERVER['HTTP_HOST'];
 
-if ($domain = 'ca-dev.fr') $filePath = 'http://ca-dev.fr/PERSO/example/';
+if ($domain == 'ca-dev.fr') $filePath = 'http://ca-dev.fr/PERSO/example/';
+elseif  ($domain == 'localhost') $filePath = 'http://localhost/example/';
+
 $asRoot = file_exists('albums');
